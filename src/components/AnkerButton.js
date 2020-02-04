dd0013a4a928fdf86912e5f9f7e5b2a278f310c0
Martin Fuster <martinfuster3@gmail.com>
2020-02-04 14:39:02 -0300

    color details

--- src/components/AnkerButton.js
+++ src/components/AnkerButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function AnkerButton(props) {
+  function handleCLick(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="a-container">
-      <a href={props.href} className="price-button">
+      <a href={props.href} className="price-button" onClick={handleCLick}>
         {props.content}
       </a>
     </div>
